b1bf84110eecca7befcd0b77ae86375b702083c0
Trifle22 <xromchenkov99@gmail.com>
2020-07-29 13:05:24 +0300

    +radio

--- scripts/radioPLayer.js
+++ scripts/radioPLayer.js
@@ -1,3 +1,52 @@
 export const radioPLayerInit = () => {
+    const radio = document.querySelector('.radio');
+    const radioNavigation = document.querySelector('.radio-navigation');
+    const radioCoverImg = document.querySelector('.radio-cover__img');
+    const radioItem = document.querySelectorAll('.radio-item');
+    const radioHeader = document.querySelector('.radio-header__big');
+    const radioStop = document.querySelector('.radio-stop');
 
+    const audio = new Audio();
+    audio.type = 'audio/aac';
+    radioStop.disabled = true;
+
+    const changeIconPlay = () => {
+        if (audio.paused) {
+            radio.classList.remove('play');
+            radioStop.classList.add('fa-play');
+            radioStop.classList.remove('fa-stop');
+        } else {
+            radio.classList.add('play');
+            radioStop.classList.add('fa-stop');
+            radioStop.classList.remove('fa-play');
+        }
+    };
+
+    const selectItem = elem => {
+        radioItem.forEach(item => item.classList.remove('select'));
+        elem.classList.add('select');
+    }
+
+    radioNavigation.addEventListener('change', event => {
+        const target = event.target;
+        const parrent = target.closest('.radio-item');
+        selectItem(parrent);
+        const title = parrent.querySelector('.radio-name').textContent;
+
+        radioHeader.textContent = title;
+        const UrlImg = parrent.querySelector('.radio-img').src;
+        radioCoverImg.src = UrlImg;
+        radioStop.disabled = false;
+        audio.src = target.dataset.radioStantion;
+        audio.play();
+        changeIconPlay();
+    });
+    radioStop.addEventListener('click', () => {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+        changeIconPlay();
+    });
 }
